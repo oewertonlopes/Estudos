a298437a4b2806435da5e99b650920fa27d108e4
oewertonlopes <85997073+oewertonlopes@users.noreply.github.com>
2022-02-14 07:40:27 -0300

    ..

---
+++ javascript/Aula13/doWhile.js
@@ -0,0 +1,7 @@
+var dorDeCabeca = 1
+
+do {
+  console.log('Infelizmente precisa tomar remédio.')
+  dorDeCabeca = dorDeCabeca+1
+} while (dorDeCabeca < 10 )
+console.log('Você Melhorou!')
---
+++ javascript/Aula13/while.js
@@ -0,0 +1,6 @@
+var dorDeCabeca = 1
+
+while (dorDeCabeca < 10 ) {
+  console.log('Infelizmente precisa tomar remédio.')
+  dorDeCabeca = dorDeCabeca+1
+} console.log('Você Melhorou!')
