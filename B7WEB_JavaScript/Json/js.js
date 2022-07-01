8317557568bcafe781705a692bfe65b8beeeca89
oewertonlopes <85997073+oewertonlopes@users.noreply.github.com>
2022-07-01 14:05:07 -0300

    01/07

---
+++ B7WEB_JavaScript/Json/js.js
@@ -0,0 +1,15 @@
+let cidadao = {
+  nome: "Ewerton",
+  sobrenome: "Lopes",
+  estetica: {
+    altura: 1.8,
+    peso: 89,
+  },
+  time: "Sport",
+  relacionamento: "Casado",
+  contagem: [1, 2, 3, 4, 5, "teste"],
+};
+
+
+//objeto {nome:caracteristica}
+//array[dado,dado,dado]
---
+++ B7WEB_JavaScript/JsonParse/js.js
@@ -0,0 +1,6 @@
+//'{"nome":"ewerton", "idade":33}';
+//json.parse() => transforma string em object
+let pessoa = '{"nome":"ewerton", "idade":33}';
+pessoa = JSON.parse('{"nome":"ewerton", "idade":33}');
+
+console.log(pessoa);
