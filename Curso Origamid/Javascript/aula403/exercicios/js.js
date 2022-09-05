44d65ec52ced503d1bb3753ab4f84b6ed1e97380
Ewerton Lopes <85997073+oewertonlopes@users.noreply.github.com>
2022-09-05 11:13:40 -0300

    05/09

---
+++ Curso Origamid/Javascript/aula403/exercicios/js.js
@@ -0,0 +1,15 @@
+// Liste 5 objetos nativos
+Object
+String
+Array
+Function
+Number
+// Liste 5 objetos do browser
+window
+alert
+addEventListener
+history
+focus
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
---
+++ Curso Origamid/Javascript/aula403/script.js
@@ -0,0 +1,8 @@
+//console.log(typeof Array.fromdf !== "undefined")
+
+
+if(typeof Array.frororm !== 'undefined'){
+  console.log('Existe.')
+} else {
+  console.log('Não existe.')
+}
