d64bc9ffe1564a98ebea9dcdbb9e71fc9c5f0a42
Ewerton Lopes <85997073+oewertonlopes@users.noreply.github.com>
2022-09-08 22:19:31 -0300

    08/09

--- Curso Origamid/Javascript/aula407/exercicios/js.js
+++ Curso Origamid/Javascript/aula407/exercicios/js.js
@@ -1,14 +1,44 @@
-// Selecione cada curso e retorne uma array
-// com objetos contendo o título, descricao,
-// aulas e horas de cada curso
+// Selecione cada curso e retorne uma array com objetos contendo o título, descricao, aulas e horas de cada curso.
 
-// Retorne uma lista com os
-// números maiores que 100
+const cursos = document.querySelectorAll(".curso");
+//console.log(cursos);
+const arrayCursos = Array.from(cursos);
+//console.log(arrayCursos);
+const objetosCurso = arrayCursos.map((curso) => {
+  const titulo = curso.querySelector("h1").innerText;
+  const descricao = curso.querySelector("p").innerText;
+  const aula = curso.querySelector(".aulas").innerHTML;
+  const hora = curso.querySelector(".horas").innerHTML;
+  return {
+    titulo,
+    descricao,
+    aula,
+    hora,
+  };
+});
+
+console.log(objetosCurso[0]);
+console.log(objetosCurso[1]);
+console.log(objetosCurso[2]);
+
+// Retorne uma lista com os números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
 
-// Verifique se Baixo faz parte
-// da lista de instrumentos e retorne true
+const maiores100 = numeros.filter((numero) => {
+  if (numero > 100) {
+    return numero;
+  }
+});
+
+console.log(maiores100);
+
+// Verifique se Baixo faz parte da lista de instrumentos e retorne true
 const instrumentos = ["Guitarra", "Baixo", "Bateria", "Teclado"];
+const temBaixo = instrumentos.some((instrumento) => {
+  return instrumento === "Baixo";
+});
+
+console.log(temBaixo);
 
 // Retorne o valor total das compras
 const compras = [
@@ -33,3 +63,15 @@ const compras = [
     preco: "R$ 10,60",
   },
 ];
+
+const valorCompras = compras.map((valor) => {
+  return +valor.preco.replace("R$", "").replace(",", ".");
+});
+
+//console.log(valorCompras);
+
+const somaValorCompras = valorCompras.reduce((acumulador, valorAtual) => {
+  return acumulador + valorAtual;
+}, 0);
+
+console.log(somaValorCompras);
