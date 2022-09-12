feb76878c6ace927811b0448f163e1001d638899
Ewerton Lopes <85997073+oewertonlopes@users.noreply.github.com>
2022-09-12 10:13:41 -0300

    12/09

---
+++ Curso Origamid/Javascript/aula411/js/modules/teste.js
@@ -0,0 +1,13 @@
+export function teste1() {
+  console.log("Isso é um teste.");
+}
+
+export function teste2() {
+  console.log("Isso também é um teste.");
+}
+
+export const senha = 442323232
+
+// se não importar, o arquivo não fica disponível
+
+const nome = 'nome completo'
---
+++ Curso Origamid/Javascript/aula411/js/script.js
@@ -0,0 +1,16 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
+import initAccordion from "./modules/init-accordion.js";
+import initTabNav from "./modules/tab-nav.js";
+//import { teste1, teste2 } from "./modules/teste.js";
+import * as teste from "./modules/teste.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+//teste1();
+//teste2();
+teste.teste1();
+teste.teste2();
+console.log(teste.senha);
