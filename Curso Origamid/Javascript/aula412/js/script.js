aa0376003e2710e7617946da6b4531c62dd2894e
Ewerton Lopes <85997073+oewertonlopes@users.noreply.github.com>
2022-09-12 13:04:55 -0300

    12/09

---
+++ Curso Origamid/Javascript/aula412/js/script.js
@@ -0,0 +1,11 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
+import initAccordion from "./modules/init-accordion.js";
+import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
